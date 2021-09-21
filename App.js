f99c60883306ef336b401bdab6d7a8cdbe4e1dbc
ellikamishra <ellikamishra009@gmail.com>
2021-09-21 13:20:30 +0530

    basics for trial

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,Image,TouchableOpacity, Button, Alert } from 'react-native';
 
 export default function App() {
-
+  const handlePress=()=> console.log("Pressed!");
   console.log("App executed");
-  let x;
-  x.toString();
+  const showAlert=()=>Alert.alert(
+    "ALERT!",
+    "Lion's app",[
+      {text:"Ikr!",onPress:()=>console.log("Ikr!")},
+      {text:"Bow Down",onPress:()=>console.log("Bow down")}
+    ]
+  )
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text onPress={handlePress}>Hello React Native</Text>
+      <TouchableOpacity onPress={handlePress}>
+      <Image source={{
+        height:300,
+        width:200,
+        uri:"https://picsum.photos/200/300"}}
+      /> 
+      </TouchableOpacity>
+       
+      <Button onPress={showAlert} title="Trial" style={styles.buttone}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,8 +32,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttone: {
+    marginBottom: 4,
+    backgroundColor: "orange"
+  }
 });
