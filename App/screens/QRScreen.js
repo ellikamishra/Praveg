1996d6eef93a82281fb413a736e92c8fa3731394
ellikamishra <ellikamishra009@gmail.com>
2021-09-26 21:26:37 +0530

    QR added and some changes

--- App/screens/QRScreen.js
+++ App/screens/QRScreen.js
@@ -1,70 +1,71 @@
 import React, { useState } from 'react';
 import { Fragment } from 'react';
-import {Text, View,StatusBar,Platform,StyleSheet, Linking} from 'react-native';
+import { Text, View, StatusBar, Platform, StyleSheet, Linking } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 
 function QRScreen(props) {
-    const [scan,setScan]=useState(false);
-    const [scanResult,setScanResult]=useState(false);
-    const [result,setResult]=useState(null);
-    const onSuccess=(e)=>{
-        Linking.openURL(e.data).catch(err=>
-            console.error("An error occured",err)
-        );
-        };
-    
-    console.log("called QR")
-    return (
-        <View style={styles.conatiner}>
-            <Fragment>
-                <StatusBar barStyle="dark-content"></StatusBar>
-                <Text> Welcome to text scanner</Text>
-                <QRCodeScanner reactivate={true} onRead={(e)=>onSuccess(e)}
-                    topContent={
-                        <Text style={styles.centerText}>
-                          Go to{' '}
-                          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-                          your computer and scan the QR code.
-                        </Text>
-                      }
-                      bottomContent={
-                        <TouchableOpacity style={styles.buttonTouchable}>
-                          <Text style={styles.buttonText}>OK. Got it!</Text>
-                        </TouchableOpacity>
-                      }
-                    ></QRCodeScanner>
-                {/* {
+  const [scan, setScan] = useState(false);
+  const [scanResult, setScanResult] = useState(false);
+  const [result, setResult] = useState(null);
+  const onSuccess = (e) => {
+    Linking.openURL(e.data).catch(err =>
+      console.error("An error occured", err)
+    );
+  };
+
+  console.log("called QR")
+  return (
+    <View style={styles.conatiner}>
+      <Fragment>
+        <StatusBar barStyle="dark-content"></StatusBar>
+        <Text> Welcome to text scanner</Text>
+        <QRCodeScanner reactivate={true} onRead={(e) => onSuccess(e)}
+          topContent={
+            <Text style={styles.centerText}>
+              Go to{' '}
+              <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+              your computer and scan the QR code.
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+          }
+        ></QRCodeScanner>
+        {/* {
                     scan&&
                 } */}
-            </Fragment>
-        </View>
-    );
+      </Fragment>
+    </View>
+  );
 
-    }
-            
+}
+
 const styles = StyleSheet.create({
-    centerText: {
-      flex: 1,
-      fontSize: 18,
-      padding: 32,
-      color: '#777'
-    },
-    conatiner:{
-        paddingTop:Platform.OS==="android"?StatusBar.currentHeight:0,
-        backgroundColor:"white"
-    },
-    textBold: {
-      fontWeight: '500',
-      color: '#000'
-    },
-    buttonText: {
-      fontSize: 21,
-      color: 'rgb(0,122,255)'
-    },
-    buttonTouchable: {
-      padding: 16
-    }
-  });
-  
-  export default QRScreen;
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  conatiner: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "white"
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
+
+export default QRScreen;
+
