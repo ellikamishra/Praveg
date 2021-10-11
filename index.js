45f95eab2e92bad58e7b93bbbd93046ca7d63474
ellikamishra <ellikamishra009@gmail.com>
2021-10-11 23:16:53 +0530

    aws amplified added for backend with specification

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
