7d23f7f3f108c78c24ec006b413b21c088c5c4cc
ellikamishra <ellikamishra009@gmail.com>
2021-10-13 11:57:49 +0530

    GraphQL API added

---
+++ src/graphql/queries.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getOrder = /* GraphQL */ `
+  query GetOrder($id: ID!) {
+    getOrder(id: $id) {
+      id
+      Customername
+      totalPrice
+      itemCount
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listOrders = /* GraphQL */ `
+  query ListOrders(
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listOrders(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        Customername
+        totalPrice
+        itemCount
+        description
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateOrder = /* GraphQL */ `
+  subscription OnCreateOrder {
+    onCreateOrder {
+      id
+      Customername
+      totalPrice
+      itemCount
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateOrder = /* GraphQL */ `
+  subscription OnUpdateOrder {
+    onUpdateOrder {
+      id
+      Customername
+      totalPrice
+      itemCount
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteOrder = /* GraphQL */ `
+  subscription OnDeleteOrder {
+    onDeleteOrder {
+      id
+      Customername
+      totalPrice
+      itemCount
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
